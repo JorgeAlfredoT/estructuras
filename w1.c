1db1fc87954afcd154015573b11c3875a49945b3
JorgeAlfredoT <jorgealfredomarino2@gmail.com>
2016-10-18 12:08:32 -0500

    updates2

--- w1.c
+++ w1.c
@@ -9,48 +9,47 @@ struct nodo{
 
 void imprimir(struct nodo *);
 struct nodo *eliminar(struct nodo *, int);
-struct nodo *posicionCualquiera(struct nodo *,int ,int);
+struct nodo *posicionInicial(struct nodo *,int ,int);
 void concatenar(struct nodo *, struct nodo *);
 
 
 int main(){
-struct nodo *cabeza,*cur;
-cabeza = malloc(sizeof(struct nodo));
-cabeza->val = 1;
-cur = cabeza;
-int e=2;
-for(; e<=4; e++){
-    struct nodo *newNodo;
-    newNodo = malloc(sizeof(struct nodo));
-    newNodo->val = e;
-    cur->sgte = newNodo;
-    cur = newNodo;
- }
- cur->sgte = NULL;
+  struct nodo *cabeza,*cur;
+  cabeza = malloc(sizeof(struct nodo));
+  cabeza->val = 1;
+  cur = cabeza;
+  int e=2;
+  for(; e<=4; e++){
+      struct nodo *newNodo;
+      newNodo = malloc(sizeof(struct nodo));
+      newNodo->val = e;
+      cur->sgte = newNodo;
+      cur = newNodo;
+   }
+   cur->sgte = NULL;
 
-imprimir(cabeza);
-cabeza=eliminar(cabeza,2);
-imprimir(cabeza);
-cabeza=posicionCualquiera(cabeza,1,36);
-imprimir(cabeza);
+  imprimir(cabeza);
+  cabeza=eliminar(cabeza,2);
+  imprimir(cabeza);
+  cabeza=posicionInicial(cabeza,1,36);
+  imprimir(cabeza);
 
-struct nodo *cabeza2,*tr;
-cabeza2=malloc(sizeof(struct nodo));
-cabeza2->val=1;
-tr=cabeza2;
-int j=2;
-for(;j<=3;j++){
-   struct nodo *newNodo;
-   newNodo=malloc(sizeof(struct nodo));
-   newNodo->val=j;
-   tr->sgte=newNodo;
-   tr=newNodo;
-}
-tr->sgte=NULL;
+  struct nodo *cabeza2,*tr;
+  cabeza2=malloc(sizeof(struct nodo));
+  cabeza2->val=1;
+  tr=cabeza2;
+  int j=2;
+  for(;j<=3;j++){
+     struct nodo *newNodo;
+     newNodo=malloc(sizeof(struct nodo));
+     newNodo->val=j;
+     tr->sgte=newNodo;
+     tr=newNodo;
+  }
+  tr->sgte=NULL;
 
-concatenar(cabeza,cabeza2);
-imprimir(cabeza);
-
+  concatenar(cabeza,cabeza2);
+  imprimir(cabeza);
 
    return 0;
  }
@@ -82,7 +81,7 @@ struct nodo *eliminar(struct nodo *cabeza, int key){
 }
 
 
-struct nodo *posicionCualquiera(struct nodo *cabeza,int posicion ,int valor){
+struct nodo *posicionInicial(struct nodo *cabeza,int posicion ,int valor){
    struct nodo *prev,*cur,*nuevo;
    nuevo=malloc(sizeof(struct nodo));
    prev=cabeza;
@@ -94,15 +93,6 @@ struct nodo *posicionCualquiera(struct nodo *cabeza,int posicion ,int valor){
       cabeza=nuevo;
       return cabeza;
    }
-   int cont=0;
-   while(cont != posicion){
-      prev=cur;
-      cur=cur->sgte;
-   }
-   prev->sgte=nuevo;
-   nuevo->sgte=cur;
-   return cabeza;
-
 }
 
 
